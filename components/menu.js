d2dc2ed86d0aa072b931ae3dd2a6b3a13cb1fb70
Cameron Kelley <cameron@kelley.vc>
2019-12-24 01:44:19 -0600

    toggle between difficulties

--- components/menu.js
+++ components/menu.js
@@ -1,8 +1,35 @@
 AFRAME.registerComponent('menu-button', {
     schema: {
-        active: { type: 'bool', default: false }
+        active: { type: 'bool', default: false },
+        index: { type: 'int' },
     },
-    init: function() {},
+    init: function() {
+        var data = this.data;
+        var el = this.el;  // <a-text>
+        var matl = el.getAttribute('material'); // this doesn't parse to an object for some reason
+        // var defaultColor = matl.color;
+        // var defaultOpacity = matl.opacity;
+        //
+        // console.log(matl);
+        // console.log('default opacity', defaultOpacity)
+
+        el.addEventListener('mouseenter', function () {
+            // el.setAttribute('material', { opacity: 1 });
+        });
+
+        el.addEventListener('mouseleave', function () {
+            // console.log('leave', defaultOpacity);
+            // console.log(defaultOpacity)
+            // el.setAttribute('material', {
+            //     opacity: defaultOpacity,
+            // });
+        });
+
+        el.addEventListener('mouseup', function () {
+            AFRAME.scenes[0].emit('setDifficulty', {difficulty: data.index});
+        })
+
+    },
     update: function (oldData) {
         console.log('updating button: ', this.data);
         this.el.setAttribute('material', {
